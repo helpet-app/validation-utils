617a9a538d72d5326fee9bfd90e7fbe44178a22a
Efim Nikitin <e.nikitin1@g.nsu.ru>
2023-04-04 15:13:54 +0700

    Add Username validation

---
+++ src/main/java/com/helpet/validation/annotation/Username.java
@@ -0,0 +1,27 @@
+package com.helpet.validation.annotation;
+
+import com.helpet.validation.validator.UsernameValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = UsernameValidator.class)
+@Target({ ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.TYPE_USE })
+@Retention(RetentionPolicy.RUNTIME)
+@Repeatable(Username.List.class)
+public @interface Username {
+    String message();
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    @Documented
+    @Target({ ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.TYPE_USE })
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface List {
+        Username[] value();
+    }
+}
---
+++ src/main/java/com/helpet/validation/validator/UsernameValidator.java
@@ -0,0 +1,22 @@
+package com.helpet.validation.validator;
+
+import com.helpet.validation.annotation.Word;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UsernameValidator implements ConstraintValidator<Word, String> {
+    @Override
+    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+        if (Objects.isNull(s)) {
+            return false;
+        }
+
+        Pattern usernamePattern = Pattern.compile("[a-z_][a-z0-9._]{3,28}[a-z_]");
+        Matcher usernameMatcher = usernamePattern.matcher(s.toLowerCase());
+        return usernameMatcher.matches();
+    }
+}
