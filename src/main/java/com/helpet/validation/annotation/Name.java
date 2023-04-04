98010ee49b2c1c44b36c2b9d1716e69c423b7380
Efim Nikitin <e.nikitin1@g.nsu.ru>
2023-04-04 17:43:35 +0700

    Add Name validation

---
+++ src/main/java/com/helpet/validation/annotation/Name.java
@@ -0,0 +1,27 @@
+package com.helpet.validation.annotation;
+
+import com.helpet.validation.validator.NameValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = NameValidator.class)
+@Target({ ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.TYPE_USE })
+@Retention(RetentionPolicy.RUNTIME)
+@Repeatable(Name.List.class)
+public @interface Name {
+    String message();
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    @Documented
+    @Target({ ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.TYPE_USE })
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface List {
+        Name[] value();
+    }
+}
---
+++ src/main/java/com/helpet/validation/validator/NameValidator.java
@@ -0,0 +1,22 @@
+package com.helpet.validation.validator;
+
+import com.helpet.validation.annotation.Word;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class NameValidator implements ConstraintValidator<Word, String> {
+    @Override
+    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+        if (Objects.isNull(s)) {
+            return false;
+        }
+
+        Pattern namePattern = Pattern.compile("[A-Za-zА-Яа-яЁё](\\s[A-Za-zА-Яа-яЁё])*");
+        Matcher nameMatcher = namePattern.matcher(s.toLowerCase());
+        return nameMatcher.matches();
+    }
+}
