ac984733ef323e7c4d73d38c14955fa7b9eefec9
Efim Nikitin <e.nikitin1@g.nsu.ru>
2023-04-06 17:20:21 +0700

    Add min length and max length to WordValidator and check valid parameters

--- src/main/java/com/helpet/validation/validator/WordValidator.java
+++ src/main/java/com/helpet/validation/validator/WordValidator.java
@@ -1,6 +1,8 @@
 package com.helpet.validation.validator;
 
 import com.helpet.validation.annotation.Word;
+import com.helpet.validation.validator.exception.MaxLengthIsLessThanMinLengthException;
+import com.helpet.validation.validator.exception.MinLengthIsTooShortException;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
@@ -9,14 +11,38 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class WordValidator implements ConstraintValidator<Word, String> {
+    private int minLength;
+
+    private int maxLength;
+
+    @Override
+    public void initialize(Word constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+        this.minLength = constraintAnnotation.minLength();
+        this.maxLength = constraintAnnotation.maxLength();
+        this.validateParameters();
+    }
+
+
     @Override
     public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
         if (Objects.isNull(s)) {
             return false;
         }
 
-        Pattern wordPattern = Pattern.compile("[A-Za-zА-Яа-яЁё\\d]+");
-        Matcher wordMatcher = wordPattern.matcher(s);
-        return wordMatcher.matches();
+        String regex = String.format("^(?=.{%d,%d}$)([A-Za-zА-Яа-яЁё\\d]+)$", minLength, maxLength);
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(s);
+        return matcher.matches();
+    }
+
+    private void validateParameters() {
+        if (minLength < 1) {
+            throw new MinLengthIsTooShortException(minLength, 1);
+        }
+
+        if (maxLength < minLength) {
+            throw new MaxLengthIsLessThanMinLengthException(minLength, maxLength);
+        }
     }
 }
