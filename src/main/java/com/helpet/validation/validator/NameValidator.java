9f7b17a63ae81f69286d3e4bc21940e7d7d3890b
Efim Nikitin <e.nikitin1@g.nsu.ru>
2023-04-06 17:19:38 +0700

    Add min length to NameValidator and check valid parameters

--- src/main/java/com/helpet/validation/validator/NameValidator.java
+++ src/main/java/com/helpet/validation/validator/NameValidator.java
@@ -1,6 +1,8 @@
 package com.helpet.validation.validator;
 
 import com.helpet.validation.annotation.Name;
+import com.helpet.validation.validator.exception.MaxLengthIsLessThanMinLengthException;
+import com.helpet.validation.validator.exception.MinLengthIsTooShortException;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
@@ -9,22 +11,37 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class NameValidator implements ConstraintValidator<Name, String> {
+    private int minLength;
+
     private int maxLength;
 
     @Override
     public void initialize(Name constraintAnnotation) {
         ConstraintValidator.super.initialize(constraintAnnotation);
+        this.minLength = constraintAnnotation.minLength();
         this.maxLength = constraintAnnotation.maxLength();
+        this.validateParameters();
     }
 
     @Override
     public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
-        if (Objects.isNull(s) || s.length() > maxLength) {
+        if (Objects.isNull(s)) {
             return false;
         }
 
-        Pattern namePattern = Pattern.compile("[A-Za-zА-Яа-яЁё]+(\\s[A-Za-zА-Яа-яЁё]+)*");
-        Matcher nameMatcher = namePattern.matcher(s);
-        return nameMatcher.matches();
+        String regex = String.format("^(?=.{%d,%d}$)([A-Za-zА-Яа-яЁё]+(\\s[A-Za-zА-Яа-яЁё]+)*)$", minLength, maxLength);
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(s);
+        return matcher.matches();
+    }
+
+    private void validateParameters() {
+        if (minLength < 1) {
+            throw new MinLengthIsTooShortException(minLength, 1);
+        }
+
+        if (maxLength < minLength) {
+            throw new MaxLengthIsLessThanMinLengthException(minLength, maxLength);
+        }
     }
 }
